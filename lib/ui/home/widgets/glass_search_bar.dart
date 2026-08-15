a90f5e36a36e7616794bf96c77b47daa6684a1fc
zkjjs <86143643+zkjjs@users.noreply.github.com>
2026-08-15 08:26:53 +0800

    fix: remove redundant search bar import

--- lib/ui/home/widgets/glass_search_bar.dart
+++ lib/ui/home/widgets/glass_search_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:guoguo/ui/components/apple_glass_surface.dart';
 
 class GlassSearchBar extends StatelessWidget {
